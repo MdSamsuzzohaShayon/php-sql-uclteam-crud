fb8370547ab0f6c447b6e5ad49de3f9e20ea262e
Md Shayon <mdshayon0@gmail.com>
2019-10-03 12:24:42 +0600

    my sql query

--- config/db.php
+++ config/db.php
@@ -1,1 +1,13 @@
 <?php
+
+//ASSIGNING VARIABLES FOR CREATING DB
+    $server = "http://localhost";
+    $user = "root";
+    $password = "1234";
+    $dbname = "cartoon_movie";
+
+//    CONNECTING TO DATABASE
+    $conn = mysqli_connect($server, $user, $password, $dbname);
+
+
+
